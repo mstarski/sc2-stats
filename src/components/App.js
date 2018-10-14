b1b45624e565888f4d97aa6d34d065faf020507d
Michal Starski <michalstarski99@gmail.com>
2018-10-14 23:53:54 +0200

    init

---
+++ src/components/App.js
@@ -0,0 +1,7 @@
+const React = require('react');
+
+module.exports = class App extends React.Component {
+    render() {
+        return <h1>Hello World!</h1>
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+const App = require('./components/App');
+
+
+ReactDom.render(<App></App>, document.getElementById("root"));
