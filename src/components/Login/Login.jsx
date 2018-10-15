bc8c79006da883c86306c433c6cdedadf8b3f625
Michal Starski <michalstarski99@gmail.com>
2018-10-15 20:06:30 +0200

    login form splitted into 2 components added webfonts.sccs and background component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,10 @@
 const React = require('react');
+const LoginForm = require('./LoginForm')
 
 
 class Login extends React.Component {
     render() {
-        return (
-            <React.Fragment>
-                <form className="login-form">
-                    <h2 class="login-form__header">Welcome to Starcraft II Stats</h2>
-                    <label className="login-form__label" htmlFor="login">
-                        Login
-                    </label>
-                    <input className="login-form__input" id="login" type="text" />
-                    <label className="login-form__label" htmlFor="password">
-                        Password
-                    </label>
-                    <input className="login-form__input" id="password" type="password" />
-                    <input value="Log in" className="login-form__submit" type="submit" />
-                </form>
-            </React.Fragment>
-        )
+        return <LoginForm></LoginForm>
     }
 }
 
