de7265f88bcc2f0aeddd26220230e903de39de8d
Michal Starski <michalstarski99@gmail.com>
2018-11-21 01:51:53 +0100

    added avatar display to the screen

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,9 +1,10 @@
 //Packages
 const React = require("react");
-const { Pane, Text, Heading } = require("evergreen-ui");
+const { Pane, Text, Heading, Avatar } = require("evergreen-ui");
 
 //Components
 const MenuIcon = require("../../dumb-components/MenuIcon/MenuIcon");
+const Loader = require("../../dumb-components/Loader/Loader");
 
 class Dashboard extends React.Component {
 	constructor(props) {
@@ -14,19 +15,48 @@ class Dashboard extends React.Component {
 		const { data, error, loading } = this.props;
 		console.log(this.props);
 		return (
-			<Pane
-				width="100vw"
-				height="8vh"
-				display="flex"
-				alignItems="center"
-				padding={10}
-				justifyContent="space-between"
-			>
-				<Heading is={"h1"} size={800}>
-					SC II Stats
-				</Heading>
-				<MenuIcon />
-			</Pane>
+			<div>
+				<Pane
+					width="100vw"
+					height="8vh"
+					display="flex"
+					alignItems="center"
+					padding={10}
+					justifyContent="space-between"
+				>
+					<Heading is={"h1"} size={800}>
+						SC II Stats
+					</Heading>
+					<MenuIcon />
+				</Pane>
+				{data ? (
+					<React.Fragment>
+						<Pane
+							width={"100vw"}
+							marginTop={50}
+							padding={20}
+							key={1}
+						>
+							<Avatar
+								src={data.summary.portrait}
+								alt="profile_portrait"
+								name={data.summary.displayName}
+								size={100}
+								marginLeft={50}
+							/>
+						</Pane>
+						<Pane display="grid" width={"100vw"} height={"62.2vh"}>
+							<Pane paddingLeft={85}>
+								<Heading is={"h2"} size={600}>
+									{data.summary.displayName}
+								</Heading>
+							</Pane>
+						</Pane>
+					</React.Fragment>
+				) : (
+					<Loader />
+				)}
+			</div>
 		);
 	}
 }
