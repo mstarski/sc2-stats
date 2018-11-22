7e40d6e4d2578ef2826817181ea36c1886295997
Michal Starski <michalstarski99@gmail.com>
2018-11-22 18:17:07 +0100

    added conditionally changing race logos in the main screen

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,47 @@
 //Packages
 const React = require("react");
 const { Pane, Text, Heading, Avatar } = require("evergreen-ui");
+const compare = require("../../utilities/compareFunction");
 
 //Components
 const MenuIcon = require("../../dumb-components/MenuIcon/MenuIcon");
-const Loader = require("../../dumb-components/Loader/Loader");
+const raceLogos = require("../../utilities/raceLogos");
 
 class Dashboard extends React.Component {
 	constructor(props) {
 		super(props);
+
+		this.raceIconStyle = this.raceIconStyle.bind(this);
+	}
+
+	componentDidMount() {
+		this.setSeasonsMainRace(this.props.data.career);
+	}
+
+	setSeasonsMainRace(career_data) {
+		const wins = {
+			zerg: career_data.zergWins,
+			terran: career_data.terranWins,
+			protoss: career_data.protossWins,
+			null: 0,
+		};
+		let result = null;
+		Object.keys(wins).forEach(race => {
+			if (wins[race] > wins[result] && wins[race] !== 0) {
+				result = race;
+			}
+		});
+		localStorage.setItem("main_race", result);
+		return result;
+	}
+
+	raceIconStyle() {
+		const race = localStorage.getItem("main_race");
+		return {
+			width: raceLogos[race].size[0],
+			height: raceLogos[race].size[1],
+			marginRight: "20px",
+		};
 	}
 
 	render() {
@@ -42,6 +75,26 @@ class Dashboard extends React.Component {
 					<Pane display="grid" width={"100vw"} height={"62.2vh"}>
 						<Pane marginLeft={60}>
 							<Heading is={"h2"} size={900}>
+								{localStorage.getItem("main_race") !==
+								"null" ? (
+									<img
+										style={this.raceIconStyle()}
+										src={
+											raceLogos[
+												localStorage.getItem(
+													"main_race"
+												)
+											].logo
+										}
+										alt="race_logo"
+									/>
+								) : (
+									<Avatar
+										size={50}
+										style={this.raceIconStyle()}
+									/>
+								)}
+
 								{data.summary.displayName}
 							</Heading>
 							<Text size={300} marginLeft="4rem">
---
+++ src/utilities/raceLogos.js
@@ -0,0 +1,17 @@
+module.exports = {
+	zerg: {
+		logo: require("../../assets/zerg_logo.png"),
+		size: [50, 50],
+	},
+	terran: {
+		logo: require("../../assets/terran_logo.png"),
+		size: [50, 50],
+	},
+	protoss: {
+		logo: require("../../assets/protoss_logo.png"),
+		size: [30, 60],
+	},
+	null: {
+		size: [50, 50],
+	},
+};
