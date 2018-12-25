3214878a9840f4566cea6b9cf96f68657627b233
Michal Starski <michalstarski99@gmail.com>
2018-12-25 14:13:31 +0100

    --amend

--- src/dumb-components/Ladder/LadderTab/LadderPanel.jsx
+++ src/dumb-components/Ladder/LadderTab/LadderPanel.jsx
@@ -4,72 +4,59 @@ import LadderPreview from "../LadderPreview/LadderPreview";
 import DataProvider from "../../../utilities/DataProvider";
 import GrandmasterPanel from "../Grandmaster/GrandmasterPanel";
 
-class LadderPanel extends React.Component {
-	constructor(props) {
-		super(props);
-	}
+function LadderPanel(props) {
+	const regionId = localStorage.getItem("regionId"),
+		region = localStorage.getItem("region"),
+		realmId = localStorage.getItem("realmId"),
+		profileId = localStorage.getItem("profileId");
+	const { currentSeason, context, highlightPlayer } = props;
+	const GrandMasterPanel = DataProvider(
+		GrandmasterPanel,
+		region,
+		`ladder/grandmaster/${regionId}`,
+		{ context: context, currentSeason, highlightPlayer }
+	);
 
-	render() {
-		const regionId = localStorage.getItem("regionId"),
-			region = localStorage.getItem("region"),
-			realmId = localStorage.getItem("realmId"),
-			profileId = localStorage.getItem("profileId");
-		const { currentSeason, context, highlightPlayer } = this.props;
-		const GrandMasterPanel = DataProvider(
-			GrandmasterPanel,
-			region,
-			`ladder/grandmaster/${regionId}`,
-			{ context: context, currentSeason, highlightPlayer }
-		);
-
-		return (
-			<React.Fragment>
-				<Pane
-					overflow="scroll"
-					padding={16}
-					background="tint1"
-					flex="1"
-				>
-					{currentSeason[0].ladder.map((ladder, index) => {
-						if (ladder.league === "GRANDMASTER")
-							context.setState({
-								isGrandmaster: true,
-							});
-						const LadderView = DataProvider(
-							LadderPreview,
-							region,
-							`profile/${regionId}/${realmId}/${profileId}/ladder/${
-								ladder.ladderId
-							}`,
-							{
-								...ladder,
-								highlightPlayer,
+	return (
+		<React.Fragment>
+			<Pane overflow="scroll" padding={16} background="tint1" flex="1">
+				{currentSeason[0].ladder.map((ladder, index) => {
+					if (ladder.league === "GRANDMASTER")
+						context.setState({
+							isGrandmaster: true,
+						});
+					const LadderView = DataProvider(
+						LadderPreview,
+						region,
+						`profile/${regionId}/${realmId}/${profileId}/ladder/${
+							ladder.ladderId
+						}`,
+						{
+							...ladder,
+							highlightPlayer,
+						}
+					);
+					return (
+						<Pane
+							key={ladder.ladderId}
+							id={`panel-${ladder.ladderName}`}
+							role="tabpanel"
+							aria-labelledby={ladder.ladderName}
+							aria-hidden={index !== context.state.selectedIndex}
+							display={
+								index === context.state.selectedIndex
+									? "block"
+									: "none"
 							}
-						);
-						return (
-							<Pane
-								key={ladder.ladderId}
-								id={`panel-${ladder.ladderName}`}
-								role="tabpanel"
-								aria-labelledby={ladder.ladderName}
-								aria-hidden={
-									index !== context.state.selectedIndex
-								}
-								display={
-									index === context.state.selectedIndex
-										? "block"
-										: "none"
-								}
-							>
-								<LadderView />
-							</Pane>
-						);
-					})}
-					{context.state.isGrandmaster ? null : <GrandMasterPanel />}
-				</Pane>
-			</React.Fragment>
-		);
-	}
+						>
+							<LadderView />
+						</Pane>
+					);
+				})}
+				{context.state.isGrandmaster ? null : <GrandMasterPanel />}
+			</Pane>
+		</React.Fragment>
+	);
 }
 
 export default LadderPanel;
