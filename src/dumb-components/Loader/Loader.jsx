f9103e4c84da5614f29abf133ebdf4568ac40631
Michał Starski <michalstarski99@gmail.com>
2019-01-16 14:37:27 +0100

    changed misspelled import

--- src/dumb-components/Loader/Loader.jsx
+++ src/dumb-components/Loader/Loader.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import LoaderGif from "../../../assets/Loading.gif";
+import LoaderGif from "../../../assets/loading.gif";
 import { Pane } from "evergreen-ui";
 
 function Loader(props) {
