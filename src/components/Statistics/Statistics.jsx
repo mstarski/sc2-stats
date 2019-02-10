75e939317020f0f17bcce7c071498c356a2df51c
Michal Starski <michalstarski99@gmail.com>
2019-02-10 13:10:54 +0100

    created tablist for statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,13 +1,46 @@
 import React from "react";
 import { withRouter } from "react-router-dom";
+import { Pane, Tablist, SidebarTab, Heading } from "evergreen-ui";
 
 class Statistics extends React.Component {
 	constructor(props) {
 		super(props);
+		this.state = {
+			tabs: ["Matchups", "Match History"],
+			selectedIndex: 0,
+		};
+		this.handleTabChange = this.handleTabChange.bind(this);
 	}
+
+	handleTabChange(index) {
+		this.setState({
+			selectedIndex: index,
+		});
+	}
+
 	render() {
 		console.log(this.props);
-		return <h1>Statistics</h1>;
+		return (
+			<React.Fragment>
+				<Heading is={"h2"} size={800} padding={"1rem"}>
+					Statistics
+				</Heading>
+				<Pane display={"flex"}>
+					<Tablist padding={"1rem"} flexBasis={240}>
+						{this.state.tabs.map((tab, id) => (
+							<SidebarTab
+								key={tab}
+								id={tab}
+								onSelect={() => this.handleTabChange(id)}
+								isSelected={id === this.state.selectedIndex}
+							>
+								{tab}
+							</SidebarTab>
+						))}
+					</Tablist>
+				</Pane>
+			</React.Fragment>
+		);
 	}
 }
 
