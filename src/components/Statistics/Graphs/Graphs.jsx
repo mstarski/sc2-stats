bf245d57016976eff91aa34349fd1392a6a1f48c
Michal Starski <michalstarski99@gmail.com>
2019-03-17 20:45:22 +0100

    small file structure refactor

--- src/components/Statistics/Graphs/Graphs.jsx
+++ src/components/Statistics/Graphs/Graphs.jsx
@@ -1,37 +1,5 @@
 import React, { useEffect, useState } from "react";
-import {
-	LineChart,
-	Line,
-	XAxis,
-	YAxis,
-	CartesianGrid,
-	Tooltip,
-} from "recharts";
-
-const renderLineChart = data => (
-	<LineChart
-		width={700}
-		height={500}
-		data={data}
-		margin={{
-			top: 5,
-			right: 30,
-			left: 20,
-			bottom: 5,
-		}}
-	>
-		<XAxis dataKey="date" />
-		<Tooltip
-			formatter={(value, name, props) => (value === 1 ? "Win" : "Lose")}
-		/>
-		<Line
-			type="linear"
-			dataKey="result"
-			stroke="#8884d8"
-			activeDot={{ r: 8 }}
-		/>
-	</LineChart>
-);
+import WinRatioGraph from "./WinRatioGraph/WinRatioGraph";
 
 function Graphs(props) {
 	const [matches, setMatches] = useState([]);
@@ -44,8 +12,7 @@ function Graphs(props) {
 		}));
 		setMatches(formattedMatches);
 	}, []);
-
-	return renderLineChart(matches);
+	return <div className="graphs">{WinRatioGraph(matches)}</div>;
 }
 
 export default Graphs;
